679acc3ac10355592136989ebdd8a45e82e74117
Tim Sears <tim@timsears.com>
2025-02-15 20:01:28 -0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,21 @@
-{ pkgs }: {
-    packages = [
-      pkgs.wolfram-engine
-      pkgs.gcc
-      #pkgs.python3
-      pkgs.jupyter
-    ];
-    env = {
-      LD_LIBRARY_PATH = "${pkgs.gcc.lib}/lib";
-    };
-  }
+{
+  description = "Wolfram Engine with Jupyter Frontend";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = pkgs.buildEnv {
+          name = "wolfram-jupyter-env";
+          paths = [
+            pkgs.wolfram-engine
+            pkgs.gcc
+            pkgs.jupyter
+          ];
+        };
